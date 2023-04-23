b636713b435194a7a409ae7f3ce17717c499b4fb
Denis Sleptsov <hurrdurrrderp@gmail.com>
2023-04-23 16:30:55 +0300

    Test start command + linter

--- src/denis_hodge_podge_bot/config.py
+++ src/denis_hodge_podge_bot/config.py
@@ -7,7 +7,8 @@ class BotConfig(BaseSettings):
     bot_token: SecretStr
 
     class Config:
-        env_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.env')
+        env_file = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                '.env')
         env_file_encoding = 'utf-8'
 
 
---
+++ tests/test_start.py
@@ -0,0 +1,20 @@
+from unittest.mock import AsyncMock
+from aiogram.types import ReplyKeyboardRemove
+import pytest
+
+from denis_hodge_podge_bot.handlers.start import start
+
+
+def get_answer_kwargs(mock):
+    args, kwargs = mock.answer.call_args_list[0]
+    assert len(args) == 0
+    return kwargs
+
+
+@pytest.mark.asyncio
+async def test_start():
+    command_mock = AsyncMock(command='start')
+    await start(command_mock)
+    kwargs = get_answer_kwargs(command_mock)
+    assert '/weather' in kwargs['text']
+    assert kwargs['reply_markup'] == ReplyKeyboardRemove()
